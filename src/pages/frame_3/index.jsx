01fb8d876c74c4f5c94a15ae32d512861c102f28
MacBook Pro <mirshod@macbook-pro.local>
2024-06-01 15:07:39 +0500

    first commit

---
+++ src/pages/frame_3/index.jsx
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div>
+      <h1>Model X</h1>
+    </div>
+  );
+};
+
+export default Index;
---
+++ src/pages/frame_4/index.jsx
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div>
+      <h1>Model Y</h1>
+    </div>
+  );
+};
+
+export default Index;
